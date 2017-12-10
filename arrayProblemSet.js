b50a936bbc4a81869363a3d10abab8acc1192291
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-09 21:53:44 -0500

    Had to start back over with the function (arr) in addition to using a for loop

--- arrayProblemSet.js
+++ arrayProblemSet.js
@@ -1,7 +1,7 @@
 // printReverse() - reverse the array
-// Created an array and print the array
-var arr = [1,2,3,4,];
 // Create a function printReverse
-arr.forEach(function(num){
-	console.log(num);
-});
+function printReverse(arr){
+	for (var i = arr.length-1; i >= 0; i--){
+		
+	}
+}
