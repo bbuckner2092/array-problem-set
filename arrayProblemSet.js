89d99da54cf093448bd108f58c62b03a82c69d36
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-09 20:54:19 -0500

    Created a valid printFunction

--- arrayProblemSet.js
+++ arrayProblemSet.js
@@ -1,3 +1,10 @@
 // printReverse() - reverse the array
+// Created an array and printed the array
 var array = [1,2,3,4];
 console.log(array);
+// Create a function printReverse
+function printReverse(){
+	array.forEach(function(){
+
+	});
+};
