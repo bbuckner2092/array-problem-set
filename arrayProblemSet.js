e06456a0f95d315a843675091faa53b6d25e2ee6
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-09 22:01:20 -0500

    Added more information to the for loop and finally have the printReverse to print out correctly

--- arrayProblemSet.js
+++ arrayProblemSet.js
@@ -1,7 +1,9 @@
 // printReverse() - reverse the array
 // Create a function printReverse
 function printReverse(arr){
-	for (var i = arr.length-1; i >= 0; i--){
-		
+	for (var i = arr.length - 1; i >= 0; i--){
+		console.log(arr[i]);
 	}
 }
+
+printReverse([3,6,2,5])
