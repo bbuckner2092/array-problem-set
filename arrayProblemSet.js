a8b2fd22372c6d1290e11a7214f947f452d0892c
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-09 21:24:04 -0500

    Deleted the var array and rewrote the code for the printReverse function

--- arrayProblemSet.js
+++ arrayProblemSet.js
@@ -1,10 +1,4 @@
 // printReverse() - reverse the array
-// Created an array and printed the array
-var array = [1,2,3,4];
-console.log(array);
+// Created an array and print the array
 // Create a function printReverse
-function printReverse(){
-	array.forEach(function(){
-
-	});
-};
+function printReverse(array) {};
